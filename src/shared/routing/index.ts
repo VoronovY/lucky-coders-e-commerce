a7d7c386e6c069fdf115a6ae1c00b9130771be06
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2023-08-03 16:46:21 +0300

    refactor: change const paths to enum in routing folder

--- src/shared/routing/index.ts
+++ src/shared/routing/index.ts
@@ -1,13 +1,13 @@
-const paths = {
-  home: '/',
-  category: '/category',
-  cart: '/cart',
-  profile: '/profile',
-  login: '/login',
-  error: '/error',
-  about: '/about',
-  product: '/product',
-  registration: '/registration',
+enum Paths {
+  home = '/',
+  category = '/category',
+  cart = '/cart',
+  profile = '/profile',
+  login = '/login',
+  error = '/error',
+  about = '/about',
+  product = '/product',
+  registration = '/registration',
 };
 
-export default paths;
+export default Paths;
