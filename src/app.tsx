d4c1066f70eedc82d8f10d061f9d4c355189b85c
VoronovY <voronov.yuri87@gmail.com>
2023-08-08 23:37:52 +0200

    fix: change name main to app #30

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
-import './main.scss';
+import './app.scss';
 import router from './app/appRouter';
 
 const root = document.getElementById('root');
