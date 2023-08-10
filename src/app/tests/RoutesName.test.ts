699929c75671ee0382fb6d99a6705f0aec717259
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2023-08-11 02:41:20 +0300

    fix: fix jest config, add new test, delete index.tsx and index.module.scss in app folder

---
+++ src/app/tests/RoutesName.test.ts
@@ -0,0 +1,16 @@
+import RoutesName from '../../shared/routing';
+
+describe('RoutesName', () => {
+  it('should have the correct route values', () => {
+    expect(RoutesName.home).toBe('/');
+    expect(RoutesName.main).toBe('/main');
+    expect(RoutesName.category).toBe('/catalog');
+    expect(RoutesName.cart).toBe('/cart');
+    expect(RoutesName.profile).toBe('/profile');
+    expect(RoutesName.login).toBe('/login');
+    expect(RoutesName.error).toBe('/error');
+    expect(RoutesName.about).toBe('/about');
+    expect(RoutesName.product).toBe('/product');
+    expect(RoutesName.registration).toBe('/registration');
+  });
+});
