8c958f34493779938c21275e332a538d88bd65c9
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2023-08-10 18:16:39 +0300

    feat: add vite-plugin-svgr package

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 //@ts-ignore
 import eslint from 'vite-plugin-eslint';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint({ exclude: ['/virtual:/, node_modules/**'] })],
+  plugins: [svgr(), react(), eslint({ exclude: ['/virtual:/, node_modules/**'] })],
 });
