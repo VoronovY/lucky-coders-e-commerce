49b83a1fee80d07030727c58b2ca88cc901f0cf1
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2023-08-14 04:28:32 +0300

    feature: add password input validation

--- src/features/authentication/login/ui/LoginForm.tsx
+++ src/features/authentication/login/ui/LoginForm.tsx
@@ -2,20 +2,43 @@ import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 
 import { Link } from 'react-router-dom';
 
+import { yupResolver } from '@hookform/resolvers/yup';
+
+import { ObjectSchema, object, string } from 'yup';
+
 import styles from './LoginForm.module.scss';
 
 import { TextInput, PasswordInput } from '../../../../shared/ui';
 import Button from '../../../../shared/ui/button/Button';
 
 import RoutesName from '../../../../shared/routing';
+import { CheckedGreenIcon, CheckedRedIcon } from '../../../../app/layouts/images';
 
+interface LoginUserFieldsScheme {
+  email: string;
+}
+
+const userSchema: ObjectSchema<LoginUserFieldsScheme> = object().shape({
+  email: string().required('Email is required'),
+  password: string()
+    .required('Password is required')
+    .min(8, 'Password must contain at least 8 characters')
+    .matches(/[A-Z]/, 'Password must contain at least one uppercase letter (A-Z)')
+    .matches(/[a-z]/, 'Password must contain at least one lowercase letter (a-z)')
+    .matches(/\d/, 'Password must contain at least one digit (0-9)'),
+});
 interface LoginUserFields {
   email: string;
-  password: string;
+  password?: string;
 }
 
 function LoginForm(): JSX.Element {
-  const { handleSubmit, control } = useForm<LoginUserFields>({
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<LoginUserFields>({
+    resolver: yupResolver(userSchema),
     mode: 'onChange',
     defaultValues: {
       email: '',
@@ -24,7 +47,7 @@ function LoginForm(): JSX.Element {
   });
 
   const onSubmit: SubmitHandler<LoginUserFields> = (data) => {
-    // console.log(data);
+    console.log(data);
     return data;
   };
 
@@ -37,15 +60,49 @@ function LoginForm(): JSX.Element {
         control={control}
         rules={{ required: true }}
         render={({ field }): JSX.Element => {
-          return <TextInput id="1" placeholder="Email" label="Email *" {...field} />;
+          return <TextInput id="1" placeholder="Email" label="Email *" {...field} error={errors.email} />;
         }}
       />
       <Controller
         name="password"
         control={control}
         rules={{ required: true }}
-        render={({ field }): JSX.Element => {
-          return <PasswordInput id="2" placeholder="Password" label="Password *" {...field} />;
+        render={({ field, fieldState }): JSX.Element => {
+          const hasUppercase = /[A-Z]/.test(field.value || '');
+          const hasLowercase = /[a-z]/.test(field.value || '');
+          const hasDigit = /\d/.test(field.value || '');
+          return (
+            <>
+              <PasswordInput id="2" placeholder="Password" label="Password *" {...field} error={errors.password} />
+              {fieldState.isDirty && errors.password && (
+                <>
+                  <span>The password must contain:</span>
+                  <div className={styles.errorList}>
+                    <div>
+                      <div className={styles.errorItem}>
+                        {errors.password && errors.password.type === 'min' ? <CheckedRedIcon /> : <CheckedGreenIcon />}
+                        <span>at least 8 characters</span>
+                      </div>
+                      <div className={styles.errorItem}>
+                        {errors.password && !hasUppercase ? <CheckedRedIcon /> : <CheckedGreenIcon />}
+                        <span>at least one uppercase letter (A-Z)</span>
+                      </div>
+                    </div>
+                    <div>
+                      <div className={styles.errorItem}>
+                        {errors.password && !hasLowercase ? <CheckedRedIcon /> : <CheckedGreenIcon />}
+                        <span>at least one lowercase letter (a-z)</span>
+                      </div>
+                      <div className={styles.errorItem}>
+                        {errors.password && !hasDigit ? <CheckedRedIcon /> : <CheckedGreenIcon />}
+                        <span>at least one digit (0-9)</span>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              )}
+            </>
+          );
         }}
       />
       <p className={styles.signUpText}>
