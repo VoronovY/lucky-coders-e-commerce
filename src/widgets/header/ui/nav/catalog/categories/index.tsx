b113b2741f4a99f757c7f6df9f9a03cf35780ca0
Julia Norwill <julianorwill@gmail.com>
2023-08-17 14:46:19 +0400

    feat: add categories and catalog button

---
+++ src/widgets/header/ui/nav/catalog/categories/index.tsx
@@ -0,0 +1,44 @@
+import { Link } from 'react-router-dom';
+
+import styles from './categories.module.scss';
+
+import RoutesName from '../../../../../../shared/routing';
+import { CrossIcon, CategoriesArrowIcon } from '../../../../../../app/layouts/images';
+
+function Categories({ setIsOpen }: { setIsOpen: (state: boolean) => void }): JSX.Element {
+  const onClick = (): void => {
+    setIsOpen(false);
+  };
+  const categoriesArr = ['All Categories', 'Pearl', 'Collectible Minerals', 'Beads', 'Magical Stones'];
+  return (
+    <div className={styles.container}>
+      <div className={styles.categoriesWrapper}>
+        <div>
+          <div className={styles.close}>
+            <button type="button" onClick={onClick} className={styles.closeButton}>
+              <CrossIcon />
+            </button>
+          </div>
+          <ul className={styles.categories}>
+            {categoriesArr.map((item, index) => {
+              return (
+                <li className={styles.categoriesItem}>
+                  <Link to={RoutesName.catalog} className={styles.link}>
+                    {item}
+                  </Link>
+                  {index > 0 ? (
+                    <button type="button" className={styles.arrow}>
+                      <CategoriesArrowIcon />
+                    </button>
+                  ) : null}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Categories;
