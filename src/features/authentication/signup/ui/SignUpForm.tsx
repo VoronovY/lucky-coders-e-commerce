e8fb40009be03be7697eb66fa91b87e37f4514cc
VoronovY <voronov.yuri87@gmail.com>
2023-08-17 20:08:11 +0200

    refactor: change sign up form

--- src/features/authentication/signup/ui/SignUpForm.tsx
+++ src/features/authentication/signup/ui/SignUpForm.tsx
@@ -1,88 +1,37 @@
-import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import { Controller, FormProvider, SubmitHandler, useForm } from 'react-hook-form';
 
 import { yupResolver } from '@hookform/resolvers/yup';
 
 import { ObjectSchema } from 'yup';
 
-import { useEffect, useState } from 'react';
-
-import axios, { AxiosResponse } from 'axios';
-
 import RoutesName from '../../../../shared/routing';
 
-import { TextInput, PasswordInput, SelectInput, DateInput, PasswordErrors } from '../../../../shared/ui';
-import { OptionInput } from '../../../../shared/ui/select/SelectInput';
+import { TextInput, PasswordInput, DateInput, PasswordErrors } from '../../../../shared/ui';
 
 import { FormText, FormWrapper } from '../../../../shared/ui/form';
 import signUpSchema from '../model/signUpSchema';
 import passwordErrorItems from '../../../../shared/constants/passwordErrorsItems';
-
-interface CountriesOption {
-  value: string;
-  label: string;
-}
+import FieldArray from '../../../../shared/ui/addressFields/AddressFields';
+import { RegisterUserFields } from '../../../../shared/types/types';
 
 const defaultValues = {
-  email: 'string',
-  password: 'string',
-  firstName: 'string',
-  lastName: 'string',
+  email: '',
+  password: '',
+  firstName: '',
+  lastName: '',
   birthDate: new Date(),
-  country: undefined,
-  city: 'string',
-  street: 'string',
-  postal: 'string',
+  address: [
+    {
+      country: undefined,
+      city: '',
+      street: '',
+      postal: '',
+    },
+  ],
 };
 
-interface RegisterUserFields {
-  email: string;
-  password: string;
-  firstName: string;
-  lastName: string;
-  birthDate: Date;
-  country: OptionInput;
-  city: string;
-  street: string;
-  postal: string;
-}
-
-interface CountriesResponse {
-  data: Countries[];
-  err: string;
-  msg: string;
-}
-
-interface Countries {
-  cities: string[];
-  country: string;
-  iso2: string;
-  iso3: string;
-}
-
 function SignUpForm(): JSX.Element {
-  const [countries, setCountries] = useState<CountriesOption[]>([]);
-  useEffect(() => {
-    axios
-      .get<CountriesResponse>('https://countriesnow.space/api/v0.1/countries/')
-      .then((response: AxiosResponse<CountriesResponse>): void => {
-        const { data } = response;
-        // console.log(data);
-        const formatedCountries: CountriesOption[] = data.data.map((el) => ({
-          label: el.country,
-          value: el.country.toLowerCase(),
-        }));
-        setCountries(formatedCountries);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
-
-  const {
-    handleSubmit,
-    control,
-    formState: { errors },
-  } = useForm<RegisterUserFields>({
+  const methods = useForm<RegisterUserFields>({
     resolver: yupResolver(signUpSchema as ObjectSchema<RegisterUserFields>),
     mode: 'onChange',
     defaultValues,
@@ -91,97 +40,96 @@ function SignUpForm(): JSX.Element {
   const onSubmit: SubmitHandler<RegisterUserFields> = (data) => {
     return data;
   };
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <FormWrapper title="Create An account" buttonText="Sign Up">
-        <Controller
-          name="email"
-          control={control}
-          render={({ field }): JSX.Element => {
-            return <TextInput id="1" placeholder="Email" label="Email *" {...field} />;
-          }}
-        />
-        <Controller
-          name="password"
-          control={control}
-          render={({ field, fieldState: { isDirty } }): JSX.Element => {
-            return (
-              <>
-                <PasswordInput id="2" placeholder="Password" label="Password *" {...field} error={errors.password} />
-                {isDirty && errors.password && (
-                  <PasswordErrors value={field.value || ''} errorItems={passwordErrorItems} />
-                )}
-              </>
-            );
-          }}
-        />
-        <Controller
-          name="firstName"
-          control={control}
-          render={({ field }): JSX.Element => {
-            return (
-              <TextInput id="3" placeholder="First name" label="First name *" error={errors.firstName} {...field} />
-            );
-          }}
-        />
-        <Controller
-          name="lastName"
-          control={control}
-          render={({ field }): JSX.Element => {
-            return <TextInput id="4" placeholder="Last name" label="Last name *" error={errors.lastName} {...field} />;
-          }}
-        />
-        <Controller
-          name="birthDate"
-          control={control}
-          render={({ field: { onChange, value } }): JSX.Element => {
-            return <DateInput id="6" title="Birth date" onChange={onChange} value={value} error={errors.birthDate} />;
-          }}
-        />
-        <Controller
-          name="country"
-          control={control}
-          render={({ field: { onChange, value }, fieldState: { error } }): JSX.Element => {
-            return (
-              <SelectInput
-                id="5"
-                placeholder="Select Country"
-                title="Select Country *"
-                options={countries}
-                onChange={onChange}
-                value={value}
-                error={error}
-              />
-            );
-          }}
-        />
-        <Controller
-          name="city"
-          control={control}
-          rules={{ required: true }}
-          render={({ field }): JSX.Element => {
-            return <TextInput id="7" placeholder="City" label="City *" {...field} />;
-          }}
-        />
-        <Controller
-          name="street"
-          control={control}
-          rules={{ required: true }}
-          render={({ field }): JSX.Element => {
-            return <TextInput id="8" placeholder="Street" label="Street *" {...field} />;
-          }}
-        />
-        <Controller
-          name="postal"
-          control={control}
-          rules={{ required: true }}
-          render={({ field }): JSX.Element => {
-            return <TextInput id="9" placeholder="Postal" label="Postal *" {...field} />;
-          }}
-        />
-        <FormText link={RoutesName.login} linkText="Sign In" text="Already have an account?" />
-      </FormWrapper>
-    </form>
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(onSubmit)}>
+        <FormWrapper title="Create An account" buttonText="Sign Up">
+          <Controller
+            name="email"
+            control={methods.control}
+            render={({ field }): JSX.Element => {
+              return (
+                <TextInput
+                  id="1"
+                  placeholder="Email"
+                  label="Email *"
+                  {...field}
+                  error={methods.formState.errors.email}
+                />
+              );
+            }}
+          />
+          <Controller
+            name="password"
+            control={methods.control}
+            render={({ field, fieldState: { isDirty } }): JSX.Element => {
+              return (
+                <>
+                  <PasswordInput
+                    id="2"
+                    placeholder="Password"
+                    label="Password *"
+                    {...field}
+                    error={methods.formState.errors.password}
+                  />
+                  {isDirty && methods.formState.errors.password && (
+                    <PasswordErrors value={field.value || ''} errorItems={passwordErrorItems} />
+                  )}
+                </>
+              );
+            }}
+          />
+          <Controller
+            name="firstName"
+            control={methods.control}
+            render={({ field }): JSX.Element => {
+              return (
+                <TextInput
+                  id="3"
+                  placeholder="First name"
+                  label="First name *"
+                  error={methods.formState.errors.firstName}
+                  {...field}
+                />
+              );
+            }}
+          />
+          <Controller
+            name="lastName"
+            control={methods.control}
+            render={({ field }): JSX.Element => {
+              return (
+                <TextInput
+                  id="4"
+                  placeholder="Last name"
+                  label="Last name *"
+                  error={methods.formState.errors.lastName}
+                  {...field}
+                />
+              );
+            }}
+          />
+          <Controller
+            name="birthDate"
+            control={methods.control}
+            render={({ field: { onChange, value } }): JSX.Element => {
+              return (
+                <DateInput
+                  id="6"
+                  title="Birth date"
+                  onChange={onChange}
+                  value={value}
+                  error={methods.formState.errors.birthDate}
+                />
+              );
+            }}
+          />
+          <FieldArray />
+          <FormText link={RoutesName.login} linkText="Sign In" text="Already have an account?" />
+        </FormWrapper>
+      </form>
+    </FormProvider>
   );
 }
 
