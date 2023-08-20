1448663b57747703f8cc850c8540fb3fc85d2f8e
Julia Norwill <julianorwill@gmail.com>
2023-08-21 03:22:26 +0400

    fix: team comments taken into account

--- src/pages/cart/ui/CartEmpty/CartEmpty.tsx
+++ src/pages/cart/ui/CartEmpty/CartEmpty.tsx
@@ -1,17 +1,23 @@
+import { Link } from 'react-router-dom';
+
 import styles from './CartEmpty.module.scss';
 
 import Button from '../../../../shared/ui/button/Button';
 
 import { CartEmptyIcon } from '../../../../app/layouts/images';
 
+import RoutesName from '../../../../shared/routing';
+
 function CartEmpty(): JSX.Element {
   return (
     <div className={styles.cart}>
       <CartEmptyIcon className={styles.cartIcon} />
       <p className={styles.cartText}>Your cart is empty.</p>
-      <Button width="300px" height="35px">
-        Go to Catalog
-      </Button>
+      <Link to={RoutesName.catalog} className={styles.linkWrapper}>
+        <Button width="300px" height="35px">
+          Go to Catalog
+        </Button>
+      </Link>
     </div>
   );
 }
