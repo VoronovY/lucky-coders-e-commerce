437b81590db90bed8413efb4e530d3890dbc65c6
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2023-08-20 17:53:16 +0300

    refactor: edit jest config and scripts to start testing coverage

--- jest.config.ts
+++ jest.config.ts
@@ -5,8 +5,19 @@ export default {
     '^.+\\.tsx?$': 'ts-jest',
   },
   moduleNameMapper: {
-    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/shared/test/__ mocks __/fileMock.js',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/src/shared/test/__mocks__/fileMock.js',
     '\\.(css|scss)$': 'identity-obj-proxy',
   },
   setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    'src/**/*.mts',
+    '!src/**/*.d.ts',
+    '!src/**/*.d.mts',
+    '!<rootDir>/node_modules/',
+    '!src/shared/api/baseApi.ts',
+    '!src/shared/api/clientBuilder/credentialsFlowClient.ts',
+    '!src/shared/api/clientBuilder/passwordFlowClient.ts',
+  ],
 };
