d32e90822ea3c950aa6ad72fcfa3ff329e7bb8df
VoronovY <voronov.yuri87@gmail.com>
2023-08-20 21:52:16 +0200

    refactor: change eslint and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 //@ts-ignore
 import eslint from 'vite-plugin-eslint';
 import svgr from 'vite-plugin-svgr';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react(), eslint({ exclude: ['/virtual:/, node_modules/**'] })],
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis',
+  plugins: [
+    svgr(),
+    react(),
+    eslint({ exclude: ['/virtual:/', '**/node_modules/**'] }),
+    nodePolyfills({
+      exclude: ['fs'],
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
       },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          process: true,
-          buffer: true,
-        }),
-        NodeModulesPolyfillPlugin(),
-      ],
-    },
+      protocolImports: true,
+    }),
+  ],
+  define: {
+    global: 'window',
   },
+
+  base: './',
 });
