0dc79e4a019bedb6d9e537733bf862dc1075fd1f
Julia Norwill <julianorwill@gmail.com>
2023-08-21 03:08:13 +0400

    fix: team comments taken into account

--- src/pages/profile/ui/profile/Profile.tsx
+++ src/pages/profile/ui/profile/Profile.tsx
@@ -1,32 +1,14 @@
 import styles from './Profile.module.scss';
 
-import { EditIcon } from '../../../../app/layouts/images';
-
-const infArr = [
-  { id: 1, tittle: 'Ivan Ivanov', inf: '' },
-  { id: 2, tittle: 'Birth Date: ', inf: '5 june, 1992' },
-  { id: 3, tittle: 'E-mail: ', inf: 'ivanov@gmail.com' },
-];
+import userImg from '../../../../../public/assets/user.png';
 
 function ProfilePage(): JSX.Element {
   return (
     <div className={styles.profile}>
       <h2>Your Account</h2>
-      <ul className={styles.profileList}>
-        {infArr.map((item) => {
-          return (
-            <li key={item.id} className={styles.inf}>
-              <button type="button" className={styles.infButton}>
-                <EditIcon />
-              </button>
-              <p className={styles.profileText}>
-                <span className={styles.infTittle}>{item.tittle}</span>
-                {item.inf}
-              </p>
-            </li>
-          );
-        })}
-      </ul>
+      <div className={styles.profileImg}>
+        <img src={userImg} alt="about" />
+      </div>
     </div>
   );
 }
