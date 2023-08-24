e24d6140769e81044fd640d624ba1cc064174bf7
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2023-08-24 18:33:08 +0300

    refactor: edit digitsRegExp and charsAndNumbersRegExp tests

--- src/app/__tests__/helpers.test.ts
+++ src/app/__tests__/helpers.test.ts
@@ -27,13 +27,13 @@ describe('getErrorMessage', () => {
 });
 
 describe('charsAndNumbersRegExp', () => {
-  it('should match strings containing only letters and numbers', () => {
+  it('must contain only letters and numbers', () => {
     expect(charsAndNumbersRegExp.test('abc123')).toBe(true);
     expect(charsAndNumbersRegExp.test('ABCXYZ789')).toBe(true);
     expect(charsAndNumbersRegExp.test('HelloWorld123')).toBe(true);
   });
 
-  it('should not match strings containing special characters', () => {
+  it('must not contain special characters', () => {
     expect(charsAndNumbersRegExp.test('!@#$%')).toBe(false);
     expect(charsAndNumbersRegExp.test('Hello, World!')).toBe(false);
     expect(charsAndNumbersRegExp.test('123-456')).toBe(false);
@@ -41,16 +41,11 @@ describe('charsAndNumbersRegExp', () => {
 });
 
 describe('digitsRegExp', () => {
-  it('should match strings containing at least one digit', () => {
+  it('must contain at least one digit', () => {
     expect(digitsRegExp.test('abc123')).toBe(true);
-    expect(digitsRegExp.test('ABCXYZ789')).toBe(true);
+    expect(digitsRegExp.test('6')).toBe(true);
     expect(digitsRegExp.test('HelloWorld123')).toBe(true);
-    expect(digitsRegExp.test('The answer is 42')).toBe(true);
-  });
-
-  it('should not match strings containing no digits', () => {
     expect(digitsRegExp.test('abc')).toBe(false);
-    expect(digitsRegExp.test('ABCXYZ')).toBe(false);
     expect(digitsRegExp.test('HelloWorld')).toBe(false);
   });
 });
