9b629092f45a900866dfc4e7a7cfbe8b0358e6c7
VoronovY <voronov.yuri87@gmail.com>
2023-08-31 16:50:21 +0200

    fix: fix scroll on change window size

--- src/pages/catalog/index.tsx
+++ src/pages/catalog/index.tsx
@@ -7,11 +7,28 @@ import styles from './CatalogPage.module.scss';
 import useScrollToTop from '../../shared/helpers/ScrollToTop';
 import { ProductList } from '../../features/productList/ui/ProductList';
 import FilterMenu from '../../widgets/filterMenu';
+import widthMobileBig from '../../shared/constants/styles';
 
 function CatalogPage(): JSX.Element {
   useScrollToTop();
 
   const [showFilters, setShowFilters] = useState(false);
+
+  const handleResizeWindow = (): void => {
+    const body = document.querySelector('body');
+    if (body) {
+      if (window.innerWidth > widthMobileBig && showFilters) {
+        setShowFilters(false);
+        body.style.overflow = 'auto';
+      }
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResizeWindow);
+
+    return () => window.removeEventListener('resize', handleResizeWindow);
+  }, [showFilters]);
 
   useEffect(() => {
     const body = document.querySelector('body');
