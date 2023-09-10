4abdb1bed4babd017bd107f65127bee1579147eb
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2023-09-11 01:08:45 +0300

    feat: add 3 promocodes

--- src/shared/ui/promocode/PromoElement.tsx
+++ src/shared/ui/promocode/PromoElement.tsx
@@ -1,19 +1,29 @@
 import styles from './PromoElements.module.scss';
 
-import Promo1Img from '../../../../public/assets/promo1.png';
+type PromoElementProps = {
+  imageSrc: string;
+  promoText: string;
+  promoCode: string;
+  backgroundColor: string;
+  codeColor: string;
+};
 
-function PromoElement(): JSX.Element {
+function PromoElement({ imageSrc, promoText, promoCode, backgroundColor, codeColor }: PromoElementProps): JSX.Element {
   return (
     <div className={styles.promoWrapper}>
-      <div className={styles.promoImg}>
-        <img src={Promo1Img} alt="Promo" />
-      </div>
-      <div className={styles.overlay} />
+      {imageSrc && (
+        <div className={styles.promoImg}>
+          <img src={imageSrc} alt="Promo" />
+        </div>
+      )}
+      <div className={styles.overlay} style={{ background: backgroundColor }} />
       <div className={styles.promoInfo}>
-        <span className={styles.promoText}>10% discount on all types of stones.</span>
+        <span className={styles.promoText}>{promoText}</span>
         <div className={styles.promoCodeWrapper}>
           <span>PROMO CODE</span>
-          <span className={styles.code}>STONE10</span>
+          <span className={styles.code} style={{ color: codeColor }}>
+            {promoCode}
+          </span>
         </div>
       </div>
     </div>
--- src/widgets/main/ui/specials/Specials.tsx
+++ src/widgets/main/ui/specials/Specials.tsx
@@ -1,17 +1,25 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+import { DiscountCode } from '@commercetools/platform-sdk';
 
 import styles from './Specials.module.scss';
 
 import { ArrowLeftIcon, ArrowRightIcon } from '../../../../app/layouts/images';
 import getDiscounts from '../../../../shared/api/discounts/getDiscounts';
 import PromoElement from '../../../../shared/ui/promocode/PromoElement';
+import Promo1Img from '../../../../../public/assets/promo1.png';
+import Promo2Img from '../../../../../public/assets/promo2.png';
+import Promo3Img from '../../../../../public/assets/promo3.png';
 
 function SpecialsContainer(): JSX.Element {
+  const [discounts, setDiscounts] = useState<DiscountCode[]>([]);
+  const [promo1, promo2, promo3] = discounts;
+
   useEffect(() => {
     getDiscounts().then((response) => {
-      console.log(response);
+      setDiscounts(response.body.results);
     });
-  });
+  }, []);
 
   return (
     <div className={styles.specialsContainer}>
@@ -20,7 +28,29 @@ function SpecialsContainer(): JSX.Element {
         <div>
           <ArrowLeftIcon className={styles.arrowNav} />
         </div>
-        <PromoElement />
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
+          <PromoElement
+            imageSrc={Promo1Img}
+            promoText={promo1?.description?.['en-US'] || ''}
+            promoCode={promo1?.name?.['en-US'] || ''}
+            backgroundColor="rgba(244, 201, 145, 0.5)"
+            codeColor="rgb(251, 121, 27)"
+          />
+          <PromoElement
+            imageSrc={Promo3Img}
+            promoText={promo2?.description?.['en-US'] || ''}
+            promoCode={promo2?.name?.['en-US'] || ''}
+            backgroundColor="rgba(199, 199, 146, 0.5)"
+            codeColor="rgb(101, 101, 43)"
+          />
+          <PromoElement
+            imageSrc={Promo2Img}
+            promoText={promo3?.description?.['en-US'] || ''}
+            promoCode={promo3?.name?.['en-US'] || ''}
+            backgroundColor="rgba(164, 175, 191, 0.5)"
+            codeColor="rgb(32, 103, 124)"
+          />
+        </div>
         <div>
           <ArrowRightIcon className={styles.arrowNav} />
         </div>
