3e6e6b48b4c1e818fa2bec60036b1c9b9521a5ef
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2023-09-12 11:59:38 +0300

    feat: display product cart list using api

--- src/entities/product/ui/CartProduct/CartProductItem.tsx
+++ src/entities/product/ui/CartProduct/CartProductItem.tsx
@@ -1,44 +1,51 @@
-import { useState } from 'react';
+import { LineItem } from '@commercetools/platform-sdk';
 
 import styles from './CartProductItem.module.scss';
 
 import { DeleteIcon } from '../../../../app/layouts/images';
-import CrystalIcon from '../../../../../public/assets/crystals.png';
 
-function CartProduct(): JSX.Element {
-  const [quantity, setQuantity] = useState(1);
+interface CartProductListProps {
+  lineItem: LineItem;
+}
 
-  const decreaseQuantity = (): void => {
-    if (quantity > 1) {
-      setQuantity(quantity - 1);
-    }
-  };
-
-  const increaseQuantity = (): void => {
-    setQuantity(quantity + 1);
-  };
+function CartProduct({ lineItem }: CartProductListProps): JSX.Element {
+  const productName = lineItem.name['en-US'];
+  const productQuantity = lineItem.quantity;
+  const totalPrice = lineItem.totalPrice.centAmount / 100;
+  const oldPrice = lineItem.price.value.centAmount / 100;
+  const discountedPrice = lineItem.price.discounted ? lineItem.price.discounted.value.centAmount / 100 : null;
 
   return (
     <div className={styles.productItem}>
       <div className={styles.productImage}>
-        <img src={CrystalIcon} alt="Crystal" />
+        {lineItem.variant.images && lineItem.variant.images.length > 0 && (
+          <img src={lineItem.variant.images[0].url} alt={productName} />
+        )}
       </div>
       <div className={styles.productInfoWrapper}>
         <div className={styles.productInfo}>
-          <div className={styles.productName}>Clear Quartz Crystal</div>
-          <div className={styles.productQuantity}>In Stock: 12</div>
+          <div className={styles.productName}>{productName}</div>
+          <div className={styles.productQuantity}>In Stock: {lineItem.variant.availability?.availableQuantity}</div>
           <div className={styles.productPrice}>
-            Price: <span className={styles.oldPrice}>5.99 €</span> 4.99 €
+            Price:&nbsp;
+            {discountedPrice ? (
+              <>
+                <span className={styles.oldPrice}>{oldPrice} €</span>
+                <span className={styles.discountedPrice}> {discountedPrice} €</span>
+              </>
+            ) : (
+              <span className={styles.discountedPrice}>{oldPrice} €</span>
+            )}
           </div>
         </div>
         <div className={styles.productPriceQuantity}>
-          <div className={styles.productPrice}>4.99 €</div>
+          <div className={styles.productPrice}>{totalPrice} €</div>
           <div className={styles.productControls}>
-            <button type="button" className={styles.button} onClick={decreaseQuantity}>
+            <button type="button" className={styles.button}>
               -
             </button>
-            <span>{quantity}</span>
-            <button type="button" className={styles.button} onClick={increaseQuantity}>
+            <span>{productQuantity}</span>
+            <button type="button" className={styles.button}>
               +
             </button>
           </div>
--- src/features/authentication/login/ui/cartProductList/CartProductList.tsx
+++ src/features/authentication/login/ui/cartProductList/CartProductList.tsx
@@ -1,13 +1,27 @@
+import { useSelector } from 'react-redux';
+
+import { Cart } from '@commercetools/platform-sdk';
+
 import styles from './CartProductList.module.scss';
 
 import CartProduct from '../../../../../entities/product/ui/CartProduct/CartProductItem';
+import { selectCart } from '../../../../../entities/cart/model/selectCart';
 
-function CartProductList(): JSX.Element {
+function CartProductList(): JSX.Element | null {
+  const currentCart: Cart | null = useSelector(selectCart);
+
+  if (!currentCart) {
+    return null;
+  }
+
+  const { lineItems } = currentCart;
+  console.log(lineItems);
+
   return (
     <div className={styles.productList}>
-      <CartProduct />
-      <CartProduct />
-      <CartProduct />
+      {lineItems.map((lineItem) => (
+        <CartProduct key={lineItem.id} lineItem={lineItem} />
+      ))}
     </div>
   );
 }
